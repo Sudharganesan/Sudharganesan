36550cca651c7b7e70e1529781bc3b25c7da689c
Sudharganesan <161978668+Sudharganesan@users.noreply.github.com>
2024-03-03 12:47:05 +0530

    Create Login.php

---
+++ Login.php
@@ -0,0 +1,53 @@
+<?php
+
+$uname = $_POST['uname'];
+$upswd = $_POST['upswd'];
+
+
+if (!empty($uname) ||empty($upswd) )
+{
+
+$host = "localhost";
+$dbusername = "root";
+$dbpassword = "";
+$dbname = "personal information";
+
+
+
+// Create connection
+$conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
+
+if (mysqli_connect_error()){
+  die('Connect Error ('. mysqli_connect_errno() .') '
+    . mysqli_connect_error());
+}
+else{
+  $SELECT = "SELECT uname From login Where uname = ? Limit ";
+  $INSERT = "INSERT Into login(uname,upswd )values(?,?)";
+
+//Prepare statement
+     $stmt = $conn->prepare($SELECT);
+     $stmt->bind_param("s", $uname);
+     $stmt->execute();
+     $stmt->bind_result($uname);
+     $stmt->store_result();
+     $rnum = $stmt->num_rows;
+
+     //checking username
+      if ($rnum==0) {
+      $stmt->close();
+      $stmt = $conn->prepare($INSERT);
+      $stmt->bind_param("ss", $uname,$upswd1);
+      $stmt->execute();
+      echo "New record inserted sucessfully";
+     } else {
+      echo "Someone already register using this email";
+     }
+     $stmt->close();
+     $conn->close();
+    }
+} else {
+ echo "All field are required";
+ die();
+}
+?>
