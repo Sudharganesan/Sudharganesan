540cdc18bd3845878791daf5428998c153dbc213
Sudharganesan <161978668+Sudharganesan@users.noreply.github.com>
2024-03-03 13:45:31 +0530

    Update Register.php

--- Register.php
+++ Register.php
@@ -1,26 +1,16 @@
 <?php
-
 $uname = $_POST['uname'];
 $email  = $_POST['email'];
 $upswd = $_POST['upswd'];
 $phoneno = $_POST['phoneno'];
 
-
-
-
 if (!empty($uname) || !empty($email)  ||!empty($upswd)|| !empty($phoneno) )
 {
-
 $host = "localhost";
 $dbusername = "root";
 $dbpassword = "";
 $dbname = "personal information";
-
-
-
-// Create connection
 $conn = new mysqli ($host, $dbusername, $dbpassword, $dbname);
-
 if (mysqli_connect_error()){
   die('Connect Error ('. mysqli_connect_errno() .') '
     . mysqli_connect_error());
@@ -28,16 +18,12 @@ if (mysqli_connect_error()){
 else{
   $SELECT = "SELECT email From register Where email = ? Limit 1";
   $INSERT = "INSERT Into register (uname , email ,upswd, phoneno )values(?,?,?,?)";
-
-//Prepare statement
-     $stmt = $conn->prepare($SELECT);
-     $stmt->bind_param("s", $email);
-     $stmt->execute();
-     $stmt->bind_result($email);
-     $stmt->store_result();
-     $rnum = $stmt->num_rows;
-
-     //checking username
+  $stmt = $conn->prepare($SELECT);
+  $stmt->bind_param("s", $email);
+  $stmt->execute();
+  $stmt->bind_result($email);
+  $stmt->store_result();
+  $rnum = $stmt->num_rows;
       if ($rnum==0) {
       $stmt->close();
       $stmt = $conn->prepare($INSERT);
